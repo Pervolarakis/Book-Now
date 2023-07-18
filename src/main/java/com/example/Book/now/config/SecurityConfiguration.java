22c2f155b19c580f799ceaa898351769c638b5d3
Michalis Pervolarakis <pervolarakes@gmail.com>
2023-07-18 12:01:51 +0300

    Initialized exception handling

--- src/main/java/com/example/Book/now/config/SecurityConfiguration.java
+++ src/main/java/com/example/Book/now/config/SecurityConfiguration.java
@@ -26,6 +26,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -47,7 +49,9 @@ public class SecurityConfiguration {
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .oauth2ResourceServer((oauth2) -> oauth2
                 .jwt(Customizer.withDefaults())
-            );
+            ).exceptionHandling(
+                (ex) -> ex.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                            .accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
         return http.build();
     }
 
---
+++ src/main/java/com/example/Book/now/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.Book.now.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleBadCredentialsException(BadCredentialsException exception){
+        return "wrong pass dude";
+    }
+
+}
